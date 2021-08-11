1fb551d46c49a5c1a5bab98c2a2209544d97e7b9
こんばんは <41790962+Bruceboy@users.noreply.github.com>
2021-08-11 07:16:24 +0100

    Update GreetingController.java
    Remove the copy and pasting tabbed imports

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -2,9 +2,9 @@ package com.example.demo;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-	import org.springframework.web.bind.annotation.GetMapping;
-	import org.springframework.web.bind.annotation.RequestParam;
-	import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 // Resource controller
 // HTTP requests: handled by a controller, @RestController
